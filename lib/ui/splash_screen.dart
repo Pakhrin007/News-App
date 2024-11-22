3b1c0284338b09cb7734607a69d9f3964de0edfb
Aryan <pakhrinp242@gmail.com>
2024-11-22 19:56:32 +0545

    splashscreen

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:news/ui/homescreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Homescreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/splash_pic.jpg",
+              fit: BoxFit.cover,
+              height: height * .5,
+              width: width * 1,
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Text(
+              "Top heading",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            SpinKitChasingDots(
+              color: Colors.blue,
+              size: 40,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
