c01f4fe234bfa45baf329e964e59da20dad538c9
Aryan <pakhrinp242@gmail.com>
2024-11-23 09:38:12 +0545

    fetching api

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
 import 'package:news/models/newschannelheadlinesmodel.dart';
 import 'package:news/view_model/news_view_model.dart';
 
@@ -11,6 +14,7 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  final format = DateFormat('MMMM dd,yyyy');
   NewsViewModel newsViewModel = NewsViewModel();
   @override
   Widget build(BuildContext context) {
@@ -46,7 +50,87 @@ class _HomescreenState extends State<Homescreen> {
                       ),
                     );
                   } else {
-                    return Container();
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.articles!.length,
+                      itemBuilder: (context, index) {
+                        DateTime dateTime = DateTime.parse(snapshot
+                            .data!.articles![index].publishedAt
+                            .toString());
+                        return SizedBox(
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Container(
+                                height: height * 0.6,
+                                width: width * .9,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: CachedNetworkImage(
+                                        imageUrl: snapshot
+                                            .data!.articles![index].urlToImage
+                                            .toString(),
+                                        fit: BoxFit.cover,
+                                        placeholder: (context, url) =>
+                                            Container(
+                                          child: spinkit2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 20,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: Container(
+                                    height: height * .22,
+                                    alignment: Alignment.bottomCenter,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: width * 0.7,
+                                          child: Text(snapshot
+                                              .data!.articles![index].title
+                                              .toString()),
+                                        ),
+                                        Spacer(),
+                                        Container(
+                                          width: width * 0.7,
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                snapshot.data!.articles![index]
+                                                    .source!.name
+                                                    .toString(),
+                                                maxLines: 3,
+                                              ),
+                                              Text(format.format(dateTime)),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    );
                   }
                 },
               ))
@@ -55,3 +139,8 @@ class _HomescreenState extends State<Homescreen> {
     );
   }
 }
+
+const spinkit2 = SpinKitFadingCircle(
+  color: Colors.blue,
+  size: 30,
+);
