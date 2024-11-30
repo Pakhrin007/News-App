6015efa52cb0afeaae737134ebaf9a50fbec4fd3
Aryan <pakhrinp242@gmail.com>
2024-11-30 08:03:41 +0545

    notification

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news/notificationservice.dart';
 
 class Nited extends StatefulWidget {
   const Nited({super.key});
@@ -10,6 +11,29 @@ class Nited extends StatefulWidget {
 class _NitedState extends State<Nited> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Row(
+            children: [],
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              Future.delayed(Duration(seconds: 20)).then((s) {
+                NotificationService().showNotification(
+                  id: 1,
+                  body: "Welcome",
+                  payload: "now",
+                  title: "New Notificatio",
+                );
+              });
+            },
+            label: Text("Show Notification"),
+            icon: Icon(Icons.notifications),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:news/homepage.dart';
+import 'package:news/notificationservice.dart';
 import 'package:news/ui/List.dart';
 // import 'package:news/ui/expansionpannelist.dart';
 import 'package:news/ui/homescreen.dart';
 import 'package:news/ui/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final NotificationService notificationService = NotificationService();
+  await notificationService.initNotification();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Local Notification',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.deepPurple,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            )),
+        primaryColor: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.deepPurple.shade200,
         useMaterial3: true,
       ),
       home: Nited(),
